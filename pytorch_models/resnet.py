2596755bbac07f60c73f27ae425c8874ada94dfa
Magikis <kulczakkacper@gmail.com>
2019-01-31 13:37:39 +0100

    _

--- pytorch_models/resnet.py
+++ pytorch_models/resnet.py
@@ -108,8 +108,8 @@ class ResNet(nn.Module):
         self.layer2 = self._make_layer(block, 128, layers[1], stride=2)
         self.layer3 = self._make_layer(block, 256, layers[2], stride=2)
         self.layer4 = self._make_layer(block, 512, layers[3], stride=2)
-        # self.avgpool = nn.AvgPool2d(7, stride=1)
-        self.avgpool = lambda x: x
+        self.avgpool = nn.AvgPool2d(4, stride=1)
+        # self.avgpool = lambda x: x
         self.fc = nn.Linear(512 * block.expansion, num_classes)
 
         for m in self.modules():
@@ -140,7 +140,7 @@ class ResNet(nn.Module):
     def forward(self, x):
         x = self.conv1(x)
         x = self.bn1(x)
-        # x = self.relu(x)
+        x = self.relu(x)
         # x = self.maxpool(x)
 
         x = self.layer1(x)
@@ -148,7 +148,7 @@ class ResNet(nn.Module):
         x = self.layer3(x)
         x = self.layer4(x)
 
-        # x = self.avgpool(x)
+        x = self.avgpool(x)
         x = x.view(x.size(0), -1)
         x = self.fc(x)
 
